ff727bce133d1459a3e5869085a295d7478b4ee6
MikeBacon <mikebacon0606@yahoo.com>
2014-11-04 00:14:45 -0800

    Second commit connect to mongo

--- test/test.py
+++ test/test.py
@@ -1,1 +1,14 @@
+#1usr/bin/python
+
+import pymongo
+
 print "Hello World"
+
+
+def main():
+    client_col = pymongo.MongoClient()['test']['peyton_test']
+    print client_col.find_one()
+
+
+if __name__=='__main__':
+    main()
